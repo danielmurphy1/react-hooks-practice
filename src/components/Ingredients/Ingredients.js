48c279faab0553751b68d72985ede191de3aa744
Dan <daniel.murphy.1@gmail.com>
2021-02-21 22:48:21 -0500

    added http request in preperation for learning about useEffect hood

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,12 +3,25 @@ import React, { useState}  from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import axios from 'axios';
 
 function Ingredients() {
   const [ ingredients, setIngredients] = useState([]); 
 
-  const addIngredientHandler = ingredient => {
-    setIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
+  const addIngredientHandler = ingredient => { 
+    // fetch('https://react-hooks-practice-62927-default-rtdb.firebaseio.com/ingredients.json', {      //posting using browser fetch
+    //   method: 'POST', 
+    //   body: JSON.stringify({ingredient}), 
+    //   headers: { 'Content-Type': 'application/json' }
+    // }).then(response => {
+    //   return response.json();
+    // }).then(responseData => {
+    //   setIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
+    // });
+    axios.post('https://react-hooks-practice-62927-default-rtdb.firebaseio.com/ingredients.json', ingredient)    //using axios
+      .then(responseData => {
+        setIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
+      })
   };
 
   const removeIngredientHandler = ingredientID => {

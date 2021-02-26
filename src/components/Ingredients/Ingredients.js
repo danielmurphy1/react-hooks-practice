197d6553c1dac1f02453ad6b6ce54654e2538368
Dan <daniel.murphy.1@gmail.com>
2021-02-25 20:58:48 -0500

    started useEffect use

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState}  from 'react';
+import React, { useState, useEffect }  from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,6 +7,43 @@ import axios from 'axios';
 
 function Ingredients() {
   const [ ingredients, setIngredients] = useState([]); 
+
+  useEffect(() => {
+    // fetch('https://react-hooks-practice-62927-default-rtdb.firebaseio.com/ingredients.json').then(
+    //   response =>response.json()).then(
+    //   responseData => {
+    //     const loadedIngredients = [];
+    //     console.log(responseData)
+
+    //     for (const key in responseData ){
+    //       loadedIngredients.push({
+    //         id: key, 
+    //         title: responseData[key].title, 
+    //         amount: responseData[key].amount
+    //       });
+    //     }
+    //     setIngredients(loadedIngredients);
+    //      console.log(loadedIngredients)
+    //   });
+    axios.get('https://react-hooks-practice-62927-default-rtdb.firebaseio.com/ingredients.json').then(
+      
+        responseData => {
+        const loadedIngredients = [];
+        console.log(responseData.data)
+        for (const key in responseData.data){
+          loadedIngredients.push({
+            id: key, 
+            title: responseData.data[key].title, 
+            amount: responseData.data[key].amount
+          })
+        }
+        setIngredients(loadedIngredients);
+      console.log(loadedIngredients)
+      }
+    )
+  }, []);
+
+  
 
   const addIngredientHandler = ingredient => { 
     // fetch('https://react-hooks-practice-62927-default-rtdb.firebaseio.com/ingredients.json', {      //posting using browser fetch
